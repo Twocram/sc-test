e5056fada2082bb0bed2a9cbf897200e33df1445
Artem <2002maslovartem@gmail.com>
2023-04-04 15:52:43 +0500

    display result in console

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <button @click="getAddress">click</button>
   </div>
 </template>
 
@@ -32,11 +33,49 @@ export default {
         console.error(e)
       }
     },
+
+    getAddress() {
+      const map = new Map();
+      this.citizens.forEach(item => {
+        const { name: cityName } = this.cities.find(city => city.id === item.city_id);
+        const { name: districtName } = item.groups.find(group => group.type === 'district');
+        const { name: streetName } = item.groups.find(group => group.type === 'street');
+        const { name } = item;
+        if (!map.has(cityName)) {
+          map.set(cityName, new Map([[districtName, new Map([[streetName, [name]]])]]));
+        } else {
+          const cityMap = map.get(cityName);
+          if (!cityMap.has(districtName)) {
+            cityMap.set(districtName, new Map([[streetName, [name]]]));
+          } else {
+            const districtMap = cityMap.get(districtName);
+            if (!districtMap.has(streetName)) {
+              districtMap.set(streetName, [name]);
+            } else {
+              districtMap.get(streetName).push(name);
+            }
+          }
+        }
+      });
+
+      map.forEach((district, city) => {
+        console.log('-', city);
+        district.forEach((street, d) => {
+          console.log(' -', d);
+          street.forEach((names, s) => {
+            console.log('  -', s);
+            names.forEach((n) => console.log('   -', n))
+          })
+        })
+      })
+
+      return map;
+    }
   },
 
   async mounted() {
     await this.getAllCities();
     await this.getAllCitizens();
-  }
+  },
 }
 </script>
